310323dece9010b13d2584c18fc4e7fe9de8f47b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-06 20:02:39 +0800

    注释

--- authorization-server/src/main/java/cn/com/xuxiaowei/authorization/server/controller/JwkRestController.java
+++ authorization-server/src/main/java/cn/com/xuxiaowei/authorization/server/controller/JwkRestController.java
@@ -3,6 +3,7 @@ package cn.com.xuxiaowei.authorization.server.controller;
 import cn.com.xuxiaowei.authorization.server.properties.RsaKeyProperties;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,13 @@ import java.security.InvalidKeyException;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Map;
 
+/**
+ * JWK
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
 @RestController
 public class JwkRestController {
 
@@ -21,6 +29,20 @@ public class JwkRestController {
         this.rsaKeyProperties = rsaKeyProperties;
     }
 
+    /**
+     * JWK
+     * <p>
+     * {
+     *     "keys": [
+     *         {
+     *             "kty": "RSA",
+     *             "e": "AQAB",
+     *             "n": "g-Vw_ExC-3ehMDelqIVISzD28ey7pqk2OIQ9MB0A1QfEHDrcioh0yRC_ip5mOutoJAyZ8YmnjFKl-XNgJ3l398CwmIuiPCZhakjYTncTXaP2L5JT3JgQJsaVtoK2XTuCdjFlcpB6wDA9O2CTilw2XMHuB0IKaCB-4Sr2pXHOEJ0"
+     *         }
+     *     ]
+     * }
+     * @return 返回 JWK
+     */
     @RequestMapping("/jwk")
     public Map<String, Object> jwk() throws InvalidKeyException {
         RSAPublicKey rsaPublicKey = rsaKeyProperties.rsaPublicKey();
